da8cda493a8093e74adffa495c1d6090652b52e6
Kev Sturman <introvertsgo@gmail.com>
2021-12-01 16:57:19 +0000

    Decemeber Tidy

--- main2.py
+++ main2.py
@@ -1,12 +1,12 @@
 from PIL import Image, ImageDraw, ImageFont
 from dates import getdays
-im = Image.open("febback.jpg").convert("RGB")
+im = Image.open("dec-back.jpg").convert("RGB")
 
 im = im.resize((2480,3508))
 d = ImageDraw.Draw(im,"RGBA")
-dates = getdays(2021,11)
-d.rectangle([(40,40),(1800,3460)],(255,255,255,180))
-d.rectangle([(1800+25,40),(2450,3460)],(255,255,255,180))
+dates = getdays(2021,12)
+d.rectangle([(40,40),(1800,3460)],(255,255,255,140))
+d.rectangle([(1800+25,40),(2450,3460)],(255,255,255,140))
 fnt = ImageFont.truetype('Harabara.ttf',100)
 for y,day in enumerate(dates):
     fill = (0,0,0,255)
@@ -18,6 +18,6 @@ for y,day in enumerate(dates):
         d.line((40,y*103+250,1800,y*103+250),fill=(0,0,0,100),width=5)
         d.line((1800+25,y*103+250,2450,y*103+250),fill=(0,0,0,100),width=5)
 fntbig = ImageFont.truetype('Harabara.ttf',160)
-d.text((60,60),"November",font=fntbig,fill=(0,0,0,255))
-d.text((1850,60),"Tasks",font=fntbig,fill=(0,0,0,255))
-im.save('nov.png', 'PNG')
+d.text((60,60),"December",font=fntbig,fill=(0,0,0,255))
+# d.text((1850,60),"Tasks",font=fntbig,fill=(0,0,0,255))
+im.save('dec.png', 'PNG')
